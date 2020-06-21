ca690619844ccaaf1222d665c3ecd7332917717a
Kamitotomi <rockyaaokage@gmail.com>
2020-06-21 17:16:26 +0900

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,7 @@ class BooksController < ApplicationController
     @user = Book.find(params[:id]).user
     @books = Book.find(params[:id])
     @book_comment = BookComment.new
+    @book_comments = @books.book_comments
   end
 
   def new
