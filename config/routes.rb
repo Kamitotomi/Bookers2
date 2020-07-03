0b7800e5e52a959cd5f9115bb6444a15f9fdb43c
vagrant <vagrant@localhost.localdomain>
2020-07-03 01:43:15 +0000

    firstcommit

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,18 @@ Rails.application.routes.draw do
 
   resources :books do
   	resources :book_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
   end
   
 
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
   
   get 'home/about' => 'home#about'
+  get '/search', to: 'search#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
